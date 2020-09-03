38dc317d954fea176db9f50948387055543a558b
Naman <naman@atnda.com>
2020-09-03 14:19:26 +0530

    added typeface font, navigation and custom toolbar

--- app/src/main/java/com/codingwithnaman/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/codingwithnaman/newsapp/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.codingwithnaman.newsapp
+package com.codingwithnaman.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codingwithnaman.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
---
+++ app/src/main/java/com/codingwithnaman/newsapp/ui/fragments/NewsDetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.codingwithnaman.newsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.codingwithnaman.newsapp.R
+
+class NewsDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_details, container, false)
+    }
+}
